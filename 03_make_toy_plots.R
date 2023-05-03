203138f7725219ad13e13791d653b405b5e113d5
Kirsten Landsiedel <kirstenlandsiedel@Kirstens-MacBook-Pro.local>
2023-05-03 10:28:16 -0700

    New code for a new figure + saved to figures file

--- 03_make_toy_plots.R
+++ 03_make_toy_plots.R
@@ -54,5 +54,37 @@ p2 <- ggplot(df, aes(x, y)) +
 ggsave(filename = "figures/Normal_Tails.png", plot = p2, device = "png", height = 6, width = 8, dpi = 1200)
 
 
+## TOY PLOT 2:
 
+# Generate the two samples
+set.seed(123)
+t_sample <- rt(n = 30, df = 5)
+norm_sample <- rnorm(n = 30)
+
+# Create a data frame of the two samples
+df <- data.frame(x = c(t_sample, norm_sample),
+                 group = c(rep("t distribution", 30), rep("normal distribution", 30)))
+
+# Calculate the ECDFs using ecdf()
+ecdf_t <- ecdf(t_sample)
+ecdf_norm <- ecdf(norm_sample)
+
+# Create a data frame of the ECDFs
+df_ecdf <- data.frame(x = seq(min(df$x), max(df$x), length.out = 100),
+                      y_t = ecdf_t(seq(min(t_sample), max(t_sample), length.out = 100)),
+                      y_norm = ecdf_norm(seq(min(norm_sample), max(norm_sample), length.out = 100)))
+
+# Reshape the data into long format
+df_ecdf_long <- tidyr::gather(df_ecdf, key = "group", value = "y", y_t, y_norm)
+
+# Create the plot
+p3 <- ggplot(data = df_ecdf_long, aes(x = x, y = y, color = group)) +
+  geom_step() +
+  scale_color_manual(values = c("red", "blue")) +
+  labs(x = "x", y = "", color = "Distribution") +
+  theme_minimal()
+
+
+# Save
+ggsave(filename = "figures/Two_sample_KS.png", plot = p3, device = "png", height = 6, width = 8, dpi = 1200)
 
